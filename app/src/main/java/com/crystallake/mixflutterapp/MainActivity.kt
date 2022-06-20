1eb687f720569dbbe7b0c6af8681f150eba8adf7
dongsheng ye <yedongsheng@codemao.cn>
2022-06-20 18:00:10 +0800

    补交代码

--- app/src/main/java/com/crystallake/mixflutterapp/MainActivity.kt
+++ app/src/main/java/com/crystallake/mixflutterapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.crystallake.mixflutterapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.crystallake.mixflutterapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var mBinding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        mBinding?.btn?.setOnClickListener {
+            val intent = Intent(this@MainActivity, MyFlutterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
