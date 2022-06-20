7365e89caf249235ea2317aa98c45fbf346dedaa
dongsheng ye <yedongsheng@codemao.cn>
2022-06-20 17:26:08 +0800

    创建项目

---
+++ app/src/main/java/com/crystallake/mixflutterapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.crystallake.mixflutterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
