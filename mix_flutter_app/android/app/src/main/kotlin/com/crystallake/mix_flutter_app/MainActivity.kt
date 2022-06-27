1a5fdea82b1e81360f4884ee16dbe29516a8059d
dongsheng ye <yedongsheng@codemao.cn>
2022-06-27 20:25:38 +0800

    Revert "提交代码"
    This reverts commit a3cb2ab5e4d0a6aa5e583e5953d4c6a436ab4341.

--- mix_flutter_app/android/app/src/main/kotlin/com/crystallake/mix_flutter_app/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.crystallake.mix_flutter_app
-
-import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.plugin.common.MethodChannel
-
-class MainActivity: FlutterActivity() {
-
-}
